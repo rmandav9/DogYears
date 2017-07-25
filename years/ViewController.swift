cd2d0c0470ccb3711ce7b513ba5be8156c878f16
Rohit Mandava <rohitmandava@Rohits-MacBook-Pro.local>
2017-07-25 14:20:48 -0500

    Initial

--- years/ViewController.swift
+++ years/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textfield: UITextField!
+    
+    
+    @IBOutlet weak var displayfield: UILabel!
+    
+    
+    @IBAction func submit(_ sender: Any)
+    {
+        if let tempVar = textfield.text
+        {
+            let age = Int (tempVar)! * 7
+        
+        displayfield.text = String(age)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
